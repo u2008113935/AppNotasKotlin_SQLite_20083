41b149d3edf0b71f155dcb1ffb93a27d8caddc8f
u2008113935 <u2008113935@gmail.com>
2024-05-25 16:43:59 -0500

    Initial commit

--- app/src/main/java/com/example/appnotaskotlin_sqlite_20083/MainActivity.kt
+++ app/src/main/java/com/example/appnotaskotlin_sqlite_20083/MainActivity.kt
@@ -4,17 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.appnotaskotlin_sqlite_20083.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db : NotasDatabaseHelper
+    private lateinit var notasAdaptador: NotasAdaptador
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = NotasDatabaseHelper(this)
+
+        notasAdaptador = NotasAdaptador(db.getAllNotas(), this)
+
+        binding.notasRv.layoutManager = LinearLayoutManager(this)
+        binding.notasRv.adapter = notasAdaptador
 
         binding.FABAgregarNota.setOnClickListener{
             //Toast.makeText(applicationContext,
@@ -25,4 +35,10 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    fun onResumen(){
+        super.onResume()
+        notasAdaptador.refrescarLista(db.getAllNotas())
+    }
+
 }
